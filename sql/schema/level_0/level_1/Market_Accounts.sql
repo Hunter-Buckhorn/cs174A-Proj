395a97b6209b9fd652751e0b3f6591a1630af930
Thien Hoang <tonyhoang2308@gmail.com>
2017-11-29 19:56:48 -0800

    Small Fix by Adding Default to Market_Accounts

--- sql/schema/level_0/level_1/Market_Accounts.sql
+++ sql/schema/level_0/level_1/Market_Accounts.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS Market_Accounts (
 	aid INT NOT NULL,
     balance DECIMAL,
     uname VARCHAR(255) NOT NULL,
-    day_of_the_month INT,
-    running_balance_sum DECIMAL,
+    day_of_the_month INT DEFAULT 1,
+    running_balance_sum DECIMAL DEFAULT 0.000,
     PRIMARY KEY (aid),
 	FOREIGN KEY (aid) REFERENCES Accounts(aid)
 	  ON DELETE CASCADE
